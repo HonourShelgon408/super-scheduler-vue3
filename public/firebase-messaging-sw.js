a5d041788b09d3b050b32d54159884b6e68ab8d3
honourshelgon <honourshelgon408@gmail.com>
2021-05-04 17:20:35 +0100

    Removal off API key

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -5,16 +5,7 @@ importScripts('https://www.gstatic.com/firebasejs/8.0.2/firebase-messaging.js');
 
 /* if you alter this firebase global variable
 * change the one located in ./src/firebase/firebase-config-details.js */
-const firebase_global = {
-    apiKey: "AIzaSyBqo_x81m26uliOXF9k8Sz3fE1ZGOXu6wU",
-    authDomain: "pwa-scheduler-a9307.firebaseapp.com",
-    databaseURL: "https://pwa-scheduler-a9307.firebaseio.com",
-    projectId: "pwa-scheduler-a9307",
-    storageBucket: "pwa-scheduler-a9307.appspot.com",
-    messagingSenderId: "418459451060",
-    appId: "1:418459451060:web:adebb639b89724c36821e1",
-    measurementId: "G-1EC78QP9Q1"
-};
+
 
 firebase.initializeApp(firebase_global);
 
